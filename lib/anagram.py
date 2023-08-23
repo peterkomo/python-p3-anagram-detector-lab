7295e518dc2b7d55d2b63f392ffb75e6a38f6f2f
peterkomo <mwangipeterkomo@gmail.com>
2023-08-23 11:16:09 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.letters = sorted(letter for letter in word)
+
+    def match(self, words):
+        matched_words = []
+
+        for word in words:
+            if sorted(letter for letter in word) == self.letters:
+                matched_words.append(word)
+
+        return matched_words
+
+
+        pass
+            
